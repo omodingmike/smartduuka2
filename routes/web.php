ad794875d496c64128f1309b75cd6aabe03866d4
mike <omodingmike@gmail.com>
2025-11-27 02:18:30 +0300

    add new routes.

--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,20 @@
 <?php
 
-    use App\Enums\Ask;
-    use App\Enums\Role as EnumRole;
-    use App\Enums\Status;
-    use App\Models\User;
     use Illuminate\Support\Facades\Route;
-    use Spatie\Permission\Models\Role;
 
     Route::get( '/' , function () {
-        $admin = User::create( [
-            'name'              => 'Omoding mike' ,
-            'email'             => 'omodingmike@gmail.com' ,
-            'phone'             => '1254875855' ,
-            'username'          => 'Omoding' ,
-            'email_verified_at' => now() ,
-            'password'          => bcrypt( '123456' ) ,
-            'status'            => Status::ACTIVE ,
-            'country_code'      => '+880' ,
-            'is_guest'          => Ask::NO
-        ] );
-        $admin->assignRole( Role::find( EnumRole::ADMIN ) );
+//        $admin = User::create( [
+//            'name'              => 'Omoding mike' ,
+//            'email'             => 'omodingmike@gmail.com' ,
+//            'phone'             => '1254875855' ,
+//            'username'          => 'Omoding' ,
+//            'email_verified_at' => now() ,
+//            'password'          => bcrypt( '123456' ) ,
+//            'status'            => Status::ACTIVE ,
+//            'country_code'      => '+880' ,
+//            'is_guest'          => Ask::NO
+//        ] );
+//        $admin->assignRole( Role::find( EnumRole::ADMIN ) );
         return [ 'Laravel' => app()->version() ];
     } );
 
