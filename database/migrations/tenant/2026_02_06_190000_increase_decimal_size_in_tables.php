d1877fa0af9ceac421816724b22522c56b1200ce
mike <omodingmike@gmail.com>
2026-02-09 10:24:32 +0300

    increase column size

--- database/migrations/tenant/2026_02_06_190000_increase_decimal_size_in_tables.php
+++ database/migrations/tenant/2026_02_06_190000_increase_decimal_size_in_tables.php
@@ -24,20 +24,20 @@ return new class extends Migration
             $table->decimal('subtotal', 20, 2)->change();
             $table->decimal('tax', 20, 2)->change();
             $table->decimal('discount', 20, 2)->change();
-            $table->decimal('shipping_charge', 20, 2)->change();
+//            $table->decimal('shipping_charge', 20, 2)->change();
             $table->decimal('total', 20, 2)->change();
         });
 
         Schema::table('stocks', function (Blueprint $table) {
-            $table->decimal('buying_price', 20, 2)->change();
-            $table->decimal('selling_price', 20, 2)->change();
+//            $table->decimal('buying_price', 20, 2)->change();
+//            $table->decimal('selling_price', 20, 2)->change();
             $table->decimal('tax', 20, 2)->change();
             $table->decimal('subtotal', 20, 2)->change();
         });
 
-        Schema::table('stock_taxes', function (Blueprint $table) {
-            $table->decimal('tax', 20, 2)->change();
-        });
+//        Schema::table('stock_taxes', function (Blueprint $table) {
+//            $table->decimal('tax', 20, 2)->change();
+//        });
 
         Schema::table('orders', function (Blueprint $table) {
             $table->decimal('subtotal', 20, 2)->change();
@@ -46,23 +46,23 @@ return new class extends Migration
             $table->decimal('shipping_charge', 20, 2)->change();
             $table->decimal('total', 20, 2)->change();
             $table->decimal('balance', 20, 2)->change();
-            $table->decimal('delivery_fee', 20, 2)->change();
+//            $table->decimal('delivery_fee', 20, 2)->change();
         });
 
         Schema::table('damages', function (Blueprint $table) {
             $table->decimal('total', 20, 2)->change();
             $table->decimal('discount', 20, 2)->change();
             $table->decimal('tax', 20, 2)->change();
-            $table->decimal('shipping_charge', 20, 2)->change();
+//            $table->decimal('shipping_charge', 20, 2)->change();
         });
 
-        Schema::table('order_purchase_payments', function (Blueprint $table) {
-            $table->decimal('amount', 20, 2)->change();
-        });
+//        Schema::table('order_purchase_payments', function (Blueprint $table) {
+//            $table->decimal('amount', 20, 2)->change();
+//        });
 
-        Schema::table('credit_deposit_purchases', function (Blueprint $table) {
-            $table->decimal('amount', 20, 2)->change();
-        });
+//        Schema::table('credit_deposit_purchases', function (Blueprint $table) {
+//            $table->decimal('amount', 20, 2)->change();
+//        });
 
         Schema::table('expenses', function (Blueprint $table) {
             $table->decimal('amount', 20, 2)->change();
@@ -76,14 +76,14 @@ return new class extends Migration
             $table->decimal('amount', 20, 2)->change();
         });
 
-        Schema::table('production_setups', function (Blueprint $table) {
-            $table->decimal('cost', 20, 2)->change();
-            $table->decimal('price', 20, 2)->change();
-        });
+//        Schema::table('production_setups', function (Blueprint $table) {
+//            $table->decimal('cost', 20, 2)->change();
+//            $table->decimal('price', 20, 2)->change();
+//        });
 
-        Schema::table('commissions', function (Blueprint $table) {
-            $table->decimal('amount', 20, 2)->change();
-        });
+//        Schema::table('commissions', function (Blueprint $table) {
+//            $table->decimal('amount', 20, 2)->change();
+//        });
 
         Schema::table('users', function (Blueprint $table) {
             $table->decimal('commission', 20, 2)->change();
