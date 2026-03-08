79e429dd75191974448127dd6f6012252dc5d626
mike <omodingmike@gmail.com>
2026-03-08 08:24:05 +0300

    fix bugs.

--- app/Enums/PaymentType.php
+++ app/Enums/PaymentType.php
@@ -4,16 +4,18 @@
 
     enum PaymentType : int
     {
-        case CREDIT  = 1;
-        case DEPOSIT = 2;
-        case CASH    = 3;
+        case CREDIT   = 1;
+        case DEPOSIT  = 2;
+        case CASH     = 3;
+        case PREORDER = 4;
 
         public function label() : string
         {
             return match ( $this ) {
-                self::CREDIT  => 'Credit' ,
-                self::DEPOSIT => 'Deposit' ,
-                self::CASH    => 'Cash' ,
+                self::CREDIT   => 'Credit' ,
+                self::DEPOSIT  => 'Deposit' ,
+                self::CASH     => 'Cash' ,
+                self::PREORDER => 'Pre-Order' ,
             };
         }
     }
