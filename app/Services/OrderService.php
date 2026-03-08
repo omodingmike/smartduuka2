1d5634822b9fb0727bde24a71e5a1cfea77bb645
mike <omodingmike@gmail.com>
2026-03-08 08:38:19 +0300

    fix bugs.

--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -532,7 +532,7 @@
                             }
 
                             // Check stock
-                            if ( $targetModel->stock < $product[ 'quantity' ] ) {
+                            if ( $targetModel->stock < $product[ 'quantity' ] && ! $is_preorder ) {
                                 $name = $is_variation ? $p->name . ' (' . $variation?->productAttributeOption?->name . ')' : $p->name;
                                 throw  new Exception( "{$name} stock not enough" );
                             }
