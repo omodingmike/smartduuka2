bd8935db8b2b957c164561cb3e9e30848ff84c25
mike <omodingmike@gmail.com>
2026-03-11 12:29:30 +0300

    work on bugs.

--- database/migrations/tenant/2023_10_02_222400_create_pos_payments_table.php
+++ database/migrations/tenant/2023_10_02_222400_create_pos_payments_table.php
@@ -7,15 +7,17 @@
     return new class extends Migration {
         public function up() : void
         {
-            Schema::create('pos_payments' , function (Blueprint $table) {
-                $table->id();
-                $table->date('date');
-                $table->string('reference_no')->nullable();
-                $table->unsignedInteger('amount')->nullable();
-                $table->unsignedInteger('order_id')->nullable();
-                $table->string('payment_method')->nullable();
-                $table->timestamps();
-            });
+            if (!Schema::hasTable('pos_payments')) {
+                Schema::create('pos_payments' , function (Blueprint $table) {
+                    $table->id();
+                    $table->date('date');
+                    $table->string('reference_no')->nullable();
+                    $table->unsignedInteger('amount')->nullable();
+                    $table->unsignedInteger('order_id')->nullable();
+                    $table->string('payment_method')->nullable();
+                    $table->timestamps();
+                });
+            }
         }
 
         public function down() : void
