35ec92cbd6e4999e311b003001f09bd5a1c35c99
mike <omodingmike@gmail.com>
2026-03-12 22:17:32 +0300

    fix new tenant migrations

--- database/migrations/tenant/2026_07_26_100003_add_price_morph_to_order_products_table.php
+++ database/migrations/tenant/2026_07_26_100003_add_price_morph_to_order_products_table.php
@@ -8,14 +8,18 @@
         public function up() : void
         {
             Schema::table( 'order_products' , function (Blueprint $table) {
-                $table->nullableMorphs( 'price' );
+                if ( ! Schema::hasColumn( 'order_products' , 'price_type' ) && ! Schema::hasColumn( 'order_products' , 'price_id' ) ) {
+                    $table->nullableMorphs( 'price' );
+                }
             } );
         }
 
         public function down() : void
         {
             Schema::table( 'order_products' , function (Blueprint $table) {
-                $table->dropMorphs( 'price' );
+                if ( Schema::hasColumn( 'order_products' , 'price_type' ) && Schema::hasColumn( 'order_products' , 'price_id' ) ) {
+                    $table->dropMorphs( 'price' );
+                }
             } );
         }
     };
