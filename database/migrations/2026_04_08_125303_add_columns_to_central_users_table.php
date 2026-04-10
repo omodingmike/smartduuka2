61a44b8ad6e2e626fb324ddba98ba71e407bddbf
mike <omodingmike@gmail.com>
2026-04-10 22:47:25 +0300

    add cashflow system

---
+++ database/migrations/2026_04_08_125303_add_columns_to_central_users_table.php
@@ -0,0 +1,44 @@
+<?php
+
+    use Illuminate\Database\Migrations\Migration;
+    use Illuminate\Database\Schema\Blueprint;
+    use Illuminate\Support\Facades\Schema;
+
+    return new class extends Migration {
+        public function up() : void
+        {
+            Schema::table( 'users' , function (Blueprint $table) {
+                $table->decimal( 'commission' , 20 , 2 )->default( 0 );
+                $table->decimal( 'commission_paid' , 20 , 2 )->default( 0 );
+                $table->string( 'phone2' )->nullable()->unique();
+                $table->string( 'type' )->nullable();
+                $table->string( 'notes' )->nullable();
+                $table->timestamp( 'last_login_date' )->nullable();
+                $table->string( 'department' )->nullable();
+                $table->boolean( 'force_reset' )->default( FALSE );
+                $table->string( 'tenant_id' )->nullable();
+                $table->boolean( 'is_reset' )->default( FALSE );
+                $table->string( 'raw_pin' )->nullable();
+                $table->string( 'global_id' )->nullable();
+            } );
+        }
+
+        public function down() : void
+        {
+            Schema::table( 'users' , function (Blueprint $table) {
+                $table->dropColumn( [
+                    'commission' ,
+                    'commission_paid' ,
+                    'phone2' ,
+                    'type' ,
+                    'notes' ,
+                    'last_login_date' ,
+                    'department' ,
+                    'force_reset' ,
+                    'tenant_id' ,
+                    'is_reset' ,
+                    'raw_pin' ,
+                ] );
+            } );
+        }
+    };
