e3f5a7eabecece3c2509c84188e766b1662f28ca
mike <omodingmike@gmail.com>
2026-04-11 20:04:14 +0300

    add sync users

--- app/Console/Commands/UpdateGlobalIds.php
+++ app/Console/Commands/UpdateGlobalIds.php
@@ -18,12 +18,14 @@
         public function handle() : void
         {
             try {
+                foreach ( User::all() as $user ) {
+                    if ( ! $user->global_id ) $user->update( [ 'global_id' => Str::uuid() ] );
+                }
                 $tenants = Tenant::all();
                 foreach ( $tenants as $tenant ) {
                     tenancy()->initialize( $tenant );
                     foreach ( User::all() as $user ) {
                         if ( ! $user->hasRole( Role::CUSTOMER ) ) $user->update( [ 'global_id' => Str::uuid() ] );
-                        if ( $user->email == 'support@smartduuka.com' ) $user->update( [ 'global_id' => Str::uuid() ] );
                     }
                     tenancy()->end();
                 }
