5ec4acb55b3324d0058e211752001b6e640b8ed9
mike <omodingmike@gmail.com>
2026-04-19 20:16:19 +0300

    add App

--- routes/app-api.php
+++ routes/app-api.php
@@ -4,15 +4,6 @@
 
     use App\Http\Controllers\Auth\Apps\AuthenticatedSessionController;
     use App\Http\Controllers\Auth\CentralLoginController;
-    use App\Http\Controllers\Cashflow\ActivityLogController;
-    use App\Http\Controllers\Cashflow\CurrencyController;
-    use App\Http\Controllers\Cashflow\DashboardController;
-    use App\Http\Controllers\Cashflow\EntityController;
-    use App\Http\Controllers\Cashflow\MotherAccountController;
-    use App\Http\Controllers\Cashflow\SettingsController;
-    use App\Http\Controllers\Cashflow\SubAccountController;
-    use App\Http\Controllers\Cashflow\TransactionCategoryController;
-    use App\Http\Controllers\Cashflow\TransactionController;
     use App\Http\Controllers\TenantController;
     use Illuminate\Support\Facades\Route;
     use Stancl\Tenancy\Middleware\InitializeTenancyByRequestData;
@@ -20,6 +11,6 @@
     Route::middleware( [ 'api' , InitializeTenancyByRequestData::class , 'auth:sanctum' ] )
          ->prefix( 'app' )->group( function () {
             Route::get( 'user' , [ CentralLoginController::class , 'me' ] );
-            Route::apiResource( 'tenants' , TenantController::class );
+            Route::apiResource( 'tenants' , TenantController::class )->names( 'app.tenants' );;
             Route::post( 'logout' , [ AuthenticatedSessionController::class , 'destroy' ] );
         } );
