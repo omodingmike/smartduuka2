bbc34f501ede5c054a482b2c854df430e236bedb
mike <omodingmike@gmail.com>
2026-05-06 10:33:23 +0300

    add demo nginx config

--- app/Http/Middleware/AfterMiddleware.php
+++ app/Http/Middleware/AfterMiddleware.php
@@ -20,7 +20,7 @@
                     $originalData[ 'currency' ]          = Settings::group( 'site' )->get( 'site_default_currency_symbol' );
                     $originalData[ 'business_id' ]       = config( 'app.business_id' );
                     $originalData[ 'print_agent_token' ] = config( 'app.print_agent_token' );
-                    $originalData[ 'is_demo' ]           = TRUE;
+                    $originalData[ 'is_demo_recheck' ]   = TRUE;
                     $response->setData( $originalData );
                 }
             }
