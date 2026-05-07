f04a4e5c35bbad653ef804763202dbfe9771c059
mike <omodingmike@gmail.com>
2026-05-07 21:21:12 +0300

    work on subscriptions.

--- app/Http/Middleware/SubscribedMiddleware.php
+++ app/Http/Middleware/SubscribedMiddleware.php
@@ -14,23 +14,23 @@
     {
         public function handle(Request $request , Closure $next) : Response
         {
-//            $tenantId = tenant( 'id' );
-//            $cacheKey = "tenant_subscription_{$tenantId}";
-//
-//            $subscription = Cache::remember( $cacheKey , now()->addMinutes( 30 ) , function () use ($tenantId) {
-//                $result = FALSE;
-//
-//                tenancy()->central( function () use ($tenantId , &$result) {
-//                    $result = TenantSubscription::where( 'expires_at' , '>=' , now() )
-//                                                ->where( 'payment_status' , '=' , SubscriptionPaymentStatus::Paid )
-//                                                ->where( 'status' , '=' , Status::ACTIVE )
-//                                                ->where( 'tenant_id' , $tenantId )
-//                                                ->exists();
-//                } );
-//
-//                return $result;
-//            } );
-//
+            $tenantId = tenant( 'id' );
+            $cacheKey = "tenant_subscription_{$tenantId}";
+
+            $subscription = Cache::remember( $cacheKey , now()->addMinutes( 30 ) , function () use ($tenantId) {
+                $result = FALSE;
+
+                tenancy()->central( function () use ($tenantId , &$result) {
+                    $result = TenantSubscription::where( 'expires_at' , '>=' , now() )
+                                                ->where( 'payment_status' , '=' , SubscriptionPaymentStatus::Paid )
+                                                ->where( 'status' , '=' , Status::ACTIVE )
+                                                ->where( 'tenant_id' , $tenantId )
+                                                ->exists();
+                } );
+
+                return $result;
+            } );
+
 //            if ( ! $subscription ) {
 //                return response()->json( [
 //                    'message' => 'Your subscription has expired. Please renew your subscription.'
